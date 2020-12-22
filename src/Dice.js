366d13009031cdde7d0ae9666f2e3c86009e0dad
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-12-22 15:38:26 -0800

    init

---
+++ src/Dice.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Dice extends Component {
+    render() {
+        return (
+            <h1>DICE</h1>
+        )
+    }
+}
+
+export default Dice
---
+++ src/RollDice.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Dice from './Dice'
+
+class RollDice extends Component {
+    render() {
+        return (
+            <div><Dice /></div>
+        )
+    }
+}
+
+export default RollDice
